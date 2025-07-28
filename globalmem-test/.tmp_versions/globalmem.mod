f91a87c66e4866ed06ba97b2b0fefbcc0c245a37
jiu917 <1485311090@qq.com>
2025-07-28 06:23:54 -0400

    add globalmem

---
+++ globalmem-test/.tmp_versions/globalmem.mod
@@ -0,0 +1,3 @@
+/home/book/100ask_imx6ull-qemu/linux-4.9.88/drivers/misc/globalmem-test/globalmem.ko
+/home/book/100ask_imx6ull-qemu/linux-4.9.88/drivers/misc/globalmem-test/globalmem.o
+
---
+++ globalmem-test/globalmem.mod.c
@@ -0,0 +1,48 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x9a454969, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xdc71537f, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0x94552302, __VMLINUX_SYMBOL_STR(cdev_del) },
+	{ 0xe5d3aaae, __VMLINUX_SYMBOL_STR(cdev_add) },
+	{ 0x8fc3936c, __VMLINUX_SYMBOL_STR(cdev_init) },
+	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
+	{ 0xf3980bb0, __VMLINUX_SYMBOL_STR(kmem_cache_alloc) },
+	{ 0x2672ab2d, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+	{ 0xd8e484f0, __VMLINUX_SYMBOL_STR(register_chrdev_region) },
+	{ 0xf4fa543b, __VMLINUX_SYMBOL_STR(arm_copy_to_user) },
+	{ 0x28cc25db, __VMLINUX_SYMBOL_STR(arm_copy_from_user) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xfa2a45e, __VMLINUX_SYMBOL_STR(__memzero) },
+	{ 0xefd6cf06, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr0) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "408232A4B3DC89DBE8FA147");
